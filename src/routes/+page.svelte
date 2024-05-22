3d57802aa4ee5b5c3d9c8301690348fb5143aac6
roey <roey132@gmail.com>
2024-05-23 00:54:31 +0300

    create basic fetch request and response

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+    export let name_input = ''
+    export let password_input  = ''
+
+    async function test() {
+    const res = await fetch('http://127.0.0.1:8080/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            password:password_input,
+            username:name_input,
+        })
+    })
+        if (!res.ok) {
+                // If the response is not OK, log the status and text
+                console.error(`Error: ${res.status} ${res.statusText}`);
+                const errorText = await res.text();
+                console.error(`Error response: ${errorText}`);
+                return;
+            }
+
+        const data = await res.json()
+	    console.log(data);
+    }
+
+</script>
+
+<input bind:value={name_input} placeholder="enter your name" />
+<input bind:value={password_input} placeholder="enter your password" />
+<button on:click={test}>test</button>
