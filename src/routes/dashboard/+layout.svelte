c01ea14362b4d168ff51ee8dd510f5ff07fe8939
roey <roey132@gmail.com>
2024-05-26 19:03:46 +0300

    change user_id check to onMount

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    onMount(async() => {
+        const userId = sessionStorage.getItem("user_id");
+        if (!userId) {
+            goto("/login")
+        }
+        
+    })
+</script>
+<slot></slot>
