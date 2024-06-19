398850a3760b3ec4036a0763621791da68a8ec7e
roey <roey132@gmail.com>
2024-06-19 15:49:43 +0300

    add search and system products to meal creation

--- src/routes/dashboard/meals/create_meal/product/+page.svelte
+++ src/routes/dashboard/meals/create_meal/product/+page.svelte
@@ -1,20 +1,55 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { loadSystemProducts, loadUserProducts } from '$lib/productsUtils';
 	import { onMount } from 'svelte';
-
+	import pkg from 'lodash';
+	import {
+		loadSystemProducts,
+		loadUserProducts,
+		sortKeysByName,
+		filterSystemProducts,
+		filterProductsSearch
+	} from '$lib/productsUtils';
+	const { debounce } = pkg;
 	let allProducts = {};
-	let orderedProducts = [];
+	let sortedKeys = [];
+	let showSystem;
+	let query = '';
 
 	onMount(async () => {
-		let systemProducts = loadSystemProducts();
-		let userProducts = loadUserProducts();
+		let systemProducts = await loadSystemProducts();
+		let userProducts = await loadUserProducts();
 
 		allProducts = { ...userProducts, ...systemProducts };
+		sortedKeys = sortKeysByName(userProducts);
 	});
+
+	const debouncedSearch = debounce(() => {
+		onSystemChange();
+	}, 200);
+
+	function handleInput() {
+		debouncedSearch();
+	}
+
+	function onSystemChange() {
+		let filteredSystemProducts = filterSystemProducts(allProducts, showSystem);
+		let filteredSearchedProducts = filterProductsSearch(filteredSystemProducts, query);
+		sortedKeys = sortKeysByName(filteredSearchedProducts);
+	}
 </script>
 
-{#each orderedProducts as productId}
+<input bind:value={query} type="text" placeholder="Search..." on:input={handleInput} />
+<br />
+<input
+	bind:checked={showSystem}
+	on:change={onSystemChange}
+	type="checkbox"
+	name="systemProducts"
+	id="systemProducts"
+/>
+<label for="systemProducts">Show system products</label>
+<br />
+{#each sortedKeys as productId}
 	<br />
 	<button
 		on:click={() => {
